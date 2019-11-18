44560d9afe83b887270f62ca4e7eb0668fce6a38
kaderhashmi <52653766+kaderhashmi@users.noreply.github.com>
2019-11-18 15:33:46 +0000

    Update request headers location for function app

--- DFC.Api.JobProfiles/Extensions/HttpRequestExtensions.cs
+++ DFC.Api.JobProfiles/Extensions/HttpRequestExtensions.cs
@@ -6,7 +6,7 @@ namespace DFC.Api.JobProfiles.Extensions
     {
         public static string GetAbsoluteUrlForRelativePath(this HttpRequest request, string relativePath = null)
         {
-            request.Headers.TryGetValue("X-Original-Url", out var apimUrl);
+            request.HttpContext.Request.Headers.TryGetValue("X-Original-Url", out var apimUrl);
 
             return string.IsNullOrEmpty(apimUrl) ? $"{request.Scheme}://{request.Host}{request.Path}/{relativePath?.TrimStart('/')}" : $"{apimUrl.ToString().TrimEnd('/')}/{relativePath?.TrimStart('/')}";
         }
