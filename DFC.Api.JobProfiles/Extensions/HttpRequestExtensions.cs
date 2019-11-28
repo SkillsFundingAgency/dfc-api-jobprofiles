ffe7b97226270a2b7f00fa7d74e17fd094433a06
kaderhashmi <52653766+kaderhashmi@users.noreply.github.com>
2019-11-28 11:53:31 +0000

    Only log specific headers (#43)

--- DFC.Api.JobProfiles/Extensions/HttpRequestExtensions.cs
+++ DFC.Api.JobProfiles/Extensions/HttpRequestExtensions.cs
@@ -1,4 +1,5 @@
-﻿using DFC.Api.JobProfiles.Common.Services;
+﻿using DFC.Api.JobProfiles.Common.Constants;
+using DFC.Api.JobProfiles.Common.Services;
 using Microsoft.ApplicationInsights.DataContracts;
 using Microsoft.AspNetCore.Http;
 using System.Text;
@@ -25,10 +26,14 @@ namespace DFC.Api.JobProfiles.Extensions
         {
             var message = new StringBuilder();
 
-            foreach (var key in request.Headers.Keys)
-            {
-                message.AppendLine($"Request Header Key: '{key}', Value: '{request.Headers[key]}'");
-            }
+            request.Headers.TryGetValue(HeaderName.ApimUrl, out var apimUrl);
+            message.AppendLine($"Request Header Key: '{HeaderName.ApimUrl}', Value: '{apimUrl}'");
+
+            request.Headers.TryGetValue(HeaderName.RequestId, out var requestId);
+            message.AppendLine($"Request Header Key: '{HeaderName.RequestId}', Value: '{requestId}'");
+
+            request.Headers.TryGetValue(HeaderName.Version, out var version);
+            message.AppendLine($"Request Header Key: '{HeaderName.Version}', Value: '{version}'");
 
             logger?.LogMessage(message.ToString(), SeverityLevel.Information);
         }
