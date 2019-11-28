da505ac29b8d02d1b18d7c5817ec91241f8aac7a
kaderhashmi <52653766+kaderhashmi@users.noreply.github.com>
2019-11-28 11:36:37 +0000

    Only log specific headers

--- DFC.Api.JobProfiles.Common/Constants/HeaderName.cs
+++ DFC.Api.JobProfiles.Common/Constants/HeaderName.cs
@@ -5,5 +5,9 @@
         public const string RequestId = "Request-Id";
 
         public const string CorrelationId = "CorrelationId";
+
+        public const string ApimUrl = "X-Forwarded-APIM-Url";
+
+        public const string Version = "version";
     }
 }
