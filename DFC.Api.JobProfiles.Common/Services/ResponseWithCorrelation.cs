1205ef409121bd25cf685a9dd9b401450bf20c95
kaderhashmi <52653766+kaderhashmi@users.noreply.github.com>
2019-12-02 11:06:16 +0000

    Added ordering of API Json response

--- DFC.Api.JobProfiles.Common/Services/ResponseWithCorrelation.cs
+++ DFC.Api.JobProfiles.Common/Services/ResponseWithCorrelation.cs
@@ -1,6 +1,8 @@
 ﻿using DFC.Api.JobProfiles.Common.Constants;
+using DFC.Api.JobProfiles.Data.ContractResolver;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Net;
 
 namespace DFC.Api.JobProfiles.Common.Services
@@ -24,8 +26,12 @@ namespace DFC.Api.JobProfiles.Common.Services
 
         public IActionResult ResponseObjectWithCorrelationId(object value)
         {
+            var settings = new JsonSerializerSettings { ContractResolver = new OrderedContractResolver() };
+            var orderedModel = JsonConvert.SerializeObject(value, Formatting.Indented, settings);
+
             AddCorrelationId();
-            return new OkObjectResult(value);
+
+            return new OkObjectResult(JsonConvert.DeserializeObject(orderedModel));
         }
 
         private void AddCorrelationId()
---
+++ DFC.Api.JobProfiles.Data/Extensions/TypeExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DFC.Api.JobProfiles.Data.Extensions
+{
+    public static class TypeExtensions
+    {
+        public static IEnumerable<Type> BaseTypesAndSelf(this Type type)
+        {
+            while (type != null)
+            {
+                yield return type;
+                type = type.BaseType;
+            }
+        }
+    }
+}
