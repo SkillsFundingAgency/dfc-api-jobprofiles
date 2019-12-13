4a53d6a7ef917c2f6f091779b0c29398141e2f8e
kaderhashmi <52653766+kaderhashmi@users.noreply.github.com>
2019-12-13 11:29:53 +0000

    Update api models swagger examples

--- DFC.Api.JobProfiles.Data/ApiModels/CareerPathAndProgression/CareerPathAndProgressionApiModel.cs
+++ DFC.Api.JobProfiles.Data/ApiModels/CareerPathAndProgression/CareerPathAndProgressionApiModel.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using DFC.Swagger.Standard.Annotations;
+using System.Collections.Generic;
 
 namespace DFC.Api.JobProfiles.Data.ApiModels.CareerPathAndProgression
 {
     public class CareerPathAndProgressionApiModel
     {
+        [Example(Description = "With experience, you could specialise in a particular area, like e-commerce or move up to a more senior role like lead programmer or project leader.")]
         public List<string> CareerPathAndProgression { get; set; }
     }
 }
--- DFC.Api.JobProfiles.Data/ApiModels/WhatItTakes/WhatItTakesApiModel.cs
+++ DFC.Api.JobProfiles.Data/ApiModels/WhatItTakes/WhatItTakesApiModel.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using DFC.Swagger.Standard.Annotations;
+using System.Collections.Generic;
 
 namespace DFC.Api.JobProfiles.Data.ApiModels.WhatItTakes
 {
     public class WhatItTakesApiModel
     {
+        [Example(Description = "to have a thorough understanding of computer systems and applications")]
         public string DigitalSkillsLevel { get; set; }
 
         public List<RelatedSkillsApiModel> Skills { get; set; }
